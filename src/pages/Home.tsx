1948c038a9a150674cd72e05de66e6fe323ec3d9
Arien Rassoul <ara269@uclive.ac.nz>
2026-03-08 22:13:24 +1300

    feature: setup home page and added desciption with tooltips

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,55 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../components/ui/tooltip";
+
 export default function Home() {
   return (
     <>
-      <main>
-        <section className="container w-3xl mx-auto text-left">
-          <h1 className="font-bold mb-6 pointer-events-none">I'm Arien</h1>
-
-          <p className="text-muted-foreground mb-8">
-            Software engineer. I love music, movies, photography, and traveling.
-          </p>
-        </section>
-      </main>
+      <section className="container w-3xl mx-auto text-left">
+        <h1 className="font-semibold mb-6 pointer-events-none">I'm Arien</h1>
+        <TooltipProvider>
+          <div id="home-description">
+            <span>Software engineer — I love </span>
+            <Tooltip key="bottom">
+              <TooltipTrigger asChild>
+                <span className="text-muted-foreground mb-8 underline cursor-pointer">
+                  music
+                </span>
+              </TooltipTrigger>
+              <TooltipContent side="bottom">
+                <p>What have I been listening to?</p>
+              </TooltipContent>
+            </Tooltip>
+            <span>, </span>
+            <Tooltip key="bottom">
+              <TooltipTrigger asChild>
+                <span className="text-muted-foreground mb-8 underline cursor-pointer">
+                  movies
+                </span>
+              </TooltipTrigger>
+              <TooltipContent side="bottom">
+                {/* FIXME: Letterboxd embed */}
+                <p>What have I been watching?</p>
+              </TooltipContent>
+            </Tooltip>
+            <span>, </span>
+            <Tooltip key="bottom">
+              <TooltipTrigger asChild>
+                <span className="text-muted-foreground mb-8 underline cursor-pointer">
+                  photography, and travelling
+                </span>
+              </TooltipTrigger>
+              <TooltipContent side="bottom" color="grey">
+                <p>Where have I taken photos?</p>
+              </TooltipContent>
+            </Tooltip>
+            <span>.</span>
+          </div>
+        </TooltipProvider>
+      </section>
     </>
   );
 }
