62328fbfbbce6a6764f75f31f8cde170dd6de4bf
Arien Rassoul <ara269@uclive.ac.nz>
2026-03-08 18:28:19 +1300

    chore: configure routes and render text

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      <main>
+        <section className="container mx-auto py-16">
+          <h1 className="text-4xl font-bold mb-6">I'm Arien</h1>
+
+          <p className="text-muted-foreground mb-8">
+            Graduate software engineer.
+          </p>
+        </section>
+      </main>
+    </>
+  );
+}
