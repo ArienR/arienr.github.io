e4f52bd3af050f297d049ccafe1e0f3385ff9a62
Arien Rassoul <ara269@uclive.ac.nz>
2026-03-08 22:09:07 +1300

    chore: added home page and changed to dark mode

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,11 @@ export default function Home() {
   return (
     <>
       <main>
-        <section className="container mx-auto py-16">
-          <h1 className="text-4xl font-bold mb-6">I'm Arien</h1>
+        <section className="container w-3xl mx-auto text-left">
+          <h1 className="font-bold mb-6 pointer-events-none">I'm Arien</h1>
 
           <p className="text-muted-foreground mb-8">
-            Graduate software engineer.
+            Software engineer. I love music, movies, photography, and traveling.
           </p>
         </section>
       </main>
